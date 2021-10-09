7266320f2a73d4182e8e37a445eb96e95a0375c3
Aidin Barakanov <aidinbarakanov@icloud.com>
2021-10-10 02:28:44 +0600

    search

--- order/views.py
+++ order/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from .permissions import *
 from .serializers import *
 from .models import *
 
@@ -8,3 +10,12 @@ from .models import *
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def get_permissions(self):
+        if self.action in ['destroy']:
+            permissions = [IsOrderAuthor]
+        elif self.action in ['create']:
+            permissions = [IsAuthenticated]
+        else:
+            permissions = [IsAdminUser]
+        return [permission() for permission in permissions]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework import status
 
 from product.serializers import *
 
@@ -9,23 +11,37 @@ from .models import *
 from .serializers import *
 
 
-# Create your views here.
 class CommentViewSet(ModelViewSet):
 	queryset = Comment.objects.all()
 	serializer_class = CommentSerializer
 
-class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['action'] = self.action
-        return context
+class ProductViewSet(ModelViewSet):
+	queryset = Product.objects.all()
+	serializer_class = ProductSerializer
+
+	def get_serializer_context(self):
+		context = super().get_serializer_context()
+		context['action'] = self.action
+		return context
+
+	@action(detail=False, methods=['get'])
+	def search(self, request, pk=None):
+		q = request.query_params.get('q')
+		queryset = self.get_queryset()
+		queryset = queryset.filter(title__icontains=q)
+		page = self.paginate_queryset(queryset)
+		if page is not None:
+			serializer = self.get_serializer(page, many=True)
+			return self.get_paginated_response(serializer.data)
+		serializer = ProductSerializer(queryset, many=True)
+		return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
 
 class RatingViewSet(ModelViewSet):
 	queryset = Rating.objects.all()
