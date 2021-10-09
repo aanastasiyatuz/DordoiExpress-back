ec766edf1cdfb5977e375e844b154ae24fbae5f6
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-10-10 00:39:52 +0600

    cart dane

--- cart_/views.py
+++ cart_/views.py
@@ -1,15 +1,15 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, redirect
 from product.models import Product
 from django.contrib.auth.decorators import login_required
 from cart.cart import Cart
+from django.http import JsonResponse
 
 @login_required(login_url="/account/login")
 def cart_add(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.add(product=product)
-    return cart.items()
+    return JsonResponse(cart.cart)
 
 
 @login_required(login_url="/account/login")
@@ -17,7 +17,7 @@ def item_clear(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.remove(product)
-    return cart.items()
+    return JsonResponse(cart.cart)
 
 
 @login_required(login_url="/account/login")
@@ -25,28 +25,28 @@ def item_increment(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.add(product=product)
-    return cart.items()
+    return JsonResponse(cart.cart)
 
 
 @login_required(login_url="/account/login")
 def item_decrement(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
-    if cart.items().get(str(product.id)) == 1:
+    if cart.cart.items().get(str(product.id)) == 1:
         cart.remove(product)
     else:
         cart.decrement(product=product)
-    return cart.items()
+    return JsonResponse(cart.cart)
 
 
 @login_required(login_url="/account/login")
 def cart_clear(request):
     cart = Cart(request)
     cart.clear()
-    return HttpResponse("Succesfully cleared a cart", status_code=200)
+    return HttpResponse("Succesfully cleared a cart", status=200)
 
 
 @login_required(login_url="/account/login")
 def cart_detail(request):
     cart = Cart(request)
-    return cart.items()
+    return JsonResponse(cart.cart)
