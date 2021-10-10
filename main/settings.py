4e0981c178d634ba0bc473d2ce7cd260ba92036c
aanastasiyatuz <aanastasiyatuz@gmail.com>
2021-10-10 09:45:04 +0600

    CORS

--- main/settings.py
+++ main/settings.py
@@ -86,6 +86,11 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://34.125.134.11"
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:3000",
+    "http://34.125.134.11"
+]
 
 TEMPLATES = [
     {
